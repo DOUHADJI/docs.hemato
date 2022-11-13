de82a617069d7cdc9544f4466272dc60bdcc1cc0
DOUHADJI <58432265+DOUHADJI@users.noreply.github.com>
2022-11-13 18:05:06 +0000

    draft version : 1.1.0

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import LayoutCpn from '../components/layouts/layout'
+import PrologueCpn from '../components/prologue'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <LayoutCpn>
-      <div>here</div>
+      <PrologueCpn />
     </LayoutCpn>
   )
 }
